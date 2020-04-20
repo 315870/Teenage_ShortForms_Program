75649c336f6dfea1ae8a138fc16b450859f5da7c
315870 <60711142+315870@users.noreply.github.com>
2020-04-20 09:22:54 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿/* Dominic Langowski 
+    4/16/2020
+    Teenage Abbreviation Program*/  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TeenageAbbreviations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        string line;
+        int positionofFirstComma = -1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("list_of_short_forms.txt");
+                while(!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    positionofFirstComma = line.IndexOf(",");
+                    ///MessageBox.Show(positionofFirstComma.ToString());
+                    string shortform = line.Substring(0, positionofFirstComma);
+                    ///MessageBox.Show(longform);
+                    string longform = line.Substring(positionofFirstComma + 1);
+                    //MessageBox.Show(shortform);
+                    
+
+                    if (txtInput.Text == shortform)
+                    {
+                        lblOutput.Content = longform;
+                        break;
+                    }
+                    else if(txtInput.Text != null)
+                    {
+                        lblOutput.Content = "Sorry, not in the database";
+                        
+                    }
+                    
+                   
+                }
+                
+
+            }
+
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+           
+        }
+    }
+}
